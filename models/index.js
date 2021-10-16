0aa0132971c6793bf1a31717488b46ef3bc1b546
Nick G <nicholas.giuliani94@gmail.com>
2021-10-16 14:56:08 -0400

    associations

--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag);
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product);
 module.exports = {
   Product,
   Category,
